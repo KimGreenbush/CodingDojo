21a4b8fc41c14604864b782ba179c8947edcd33e
Kim G <Kim_greenbush@icloud.com>
2020-09-27 15:27:35 -0700

    Fundamentals 1

---
+++ C#DOTNET/Fundamentals1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Fundamentals1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // // 1 to 255
+            // for (short i = 1; i <= 255; i++){
+            //     Console.WriteLine(i);
+            // }
+            // // 1 to 255 - while
+            // short i = 1;
+            // while(i <= 255){
+            //     Console.WriteLine(i);
+            //     i++;
+            // }
+            // // 1 to 100 div by 3 OR 5
+            // for (short i = 1; i <= 100; i++){
+            //     if (i % 15 != 0){
+            //         if (i % 3 == 0 || i % 5 == 0){
+            //             Console.WriteLine(i);
+            //         }
+            //     }
+            // }
+            // // 1 to 100 div by 3 OR 5 - while
+            // short i = 1;
+            // while(i <= 100){
+            //     if (i % 15 != 0){
+            //         if (i % 3 == 0 || i % 5 == 0){
+            //             Console.WriteLine(i);
+            //         }
+            //     }
+            //     i++;
+            // }
+            // // FizzBuzz 3,5,15
+            // for (short i = 1; i <= 100; i++){
+            //     if (i % 15 == 0){
+            //         Console.WriteLine("FizzBuzz");
+            //     }
+            //     if (i % 3 == 0 && i % 5 != 0){
+            //         Console.WriteLine("Fizz");
+            //     }
+            //     if (i % 3 != 0 && i % 5 == 0){
+            //         Console.WriteLine("Buzz");
+            //     }
+            // }
+            // // FizzBuzz 3,5,15 - while
+            // short i = 1;
+            // while(i <= 100){
+            //     if (i % 15 == 0){
+            //         Console.WriteLine("FizzBuzz");
+            //     }
+            //     if (i % 3 == 0 && i % 5 != 0){
+            //         Console.WriteLine("Fizz");
+            //     }
+            //     if (i % 3 != 0 && i % 5 == 0){
+            //         Console.WriteLine("Buzz");
+            //     }
+            //     i++;
+            // }
+        }
+    }
+}
