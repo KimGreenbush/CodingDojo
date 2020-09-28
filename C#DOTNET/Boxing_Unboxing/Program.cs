6c48e94d64b138c8a9382ce7a7e0dfad76f31605
Kim G <Kim_greenbush@icloud.com>
2020-09-27 19:51:35 -0700

    Box/Unbox

---
+++ C#DOTNET/Boxing_Unboxing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Boxing_Unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object>  aBox= new List<object> ();
+            aBox.Add(7);
+            aBox.Add(28);
+            aBox.Add(-1);
+            aBox.Add(true);
+            aBox.Add("chair");
+            int total = 0;
+            foreach(var item in aBox){
+            Console.WriteLine(item);
+                if (item is int){
+                total += (int)item;
+                }
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
