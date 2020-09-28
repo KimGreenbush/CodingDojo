f65ac231f0e6fe8e7bd13771615aa6d03251ed15
Kim G <Kim_greenbush@icloud.com>
2020-09-28 13:55:17 -0700

    9/28 algos, OOP - Human

---
+++ C#DOTNET/OOP/Human/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Human
+{
+    class Human
+    {
+        // Fields for Human
+        public string Name;
+        public int Strength;
+        public int Intelligence;
+        public int Dexterity;
+        private int health;
+
+        // add a public "getter" property to access health
+        public int Health
+        {
+            get { return health; }
+        }
+
+        // Add a constructor that takes a value to set Name, and set the remaining fields to default values
+        public Human(string name){
+            Name = name;
+            Strength = 3;
+            Intelligence = 3;
+            Dexterity = 3;
+            health = 100;
+        }
+
+
+        // Add a constructor to assign custom values to all fields
+        public Human(string name, int strength, int intelligence, int dexterity, int healthspan){
+            Name = name;
+            Strength = strength;
+            Intelligence = intelligence;
+            Dexterity = dexterity;
+            health = healthspan;
+        }
+
+        // Build Attack method
+        public int Attack(Human target)
+        {
+            int damage = 5 * Strength;
+            return target.Health - damage;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Human kim = new Human("Kim");
+            Console.WriteLine(kim);
+        }
+    }
+}
--- morning_algos.js
+++ morning_algos.js
@@ -1107,3 +1107,93 @@ console.log()
 
 // add params if needed for recursion
 function floodFill(canvas2D, startXY, newColor) {}
+
+
+//C#DOTNET ---9/28---
+// Class for the node of our singly linked list
+class SLNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+// How do we actually create a new node?
+// let mynode = new SLNode(10);
+// console.log(mynode)
+
+// Class for a singly linked list
+class SLList {
+  constructor() {
+    // The only attribute we actually need is the head of the list
+    this.head = null;
+  }
+
+  // This method should return a boolean
+  // True if the list is empty, false if it is not
+  isEmpty() {
+    return (this.head == null);
+  }
+  
+  
+  //h->[1]->[3]->[7]->[]
+  
+  addToBack(value) {
+    if (isEmpty() == true){ this.head = new SLNode(value);} 
+
+    // // Assign the head of the list to a variable called runner ✅
+    let runner = this.head;
+  
+    // // to move the runner down the line ✅
+    runner = runner.next;
+
+    // // if we want to reach the LAST node: ✅
+    while(runner.next != null){
+        runner = runner.next;
+    }
+
+    // // if we want to run through ALL nodes, and be left sitting at null ✅
+    while(runner != null) { 
+      runner.next = new SLNode(value);
+    }
+
+    return this;
+  }
+
+  // BONUS:
+  // Write a method that takes an array as a parameter, and converts it into a singly linked list
+
+  // EXAMPLE:
+  // seedFromArr([1,2,3,4]) would return 
+  // 1 -> 2 -> 3 -> 4 ->
+  seedFromArr(array) {
+    for(i = 0; i < array.length; i++){
+      this.addToBack(array[i]);
+    }
+    return this;
+  }
+
+  printList() {
+    if (this.head == null) {
+      console.log("List is empty.");
+      return;
+    }
+    let output = '';
+    let runner = this.head;
+    while (runner != null) {
+      output += `${runner.value} -> `;
+      runner = runner.next;
+    }
+    console.log(output);
+    return;
+  }
+}
+
+let myList = new SLList();
+
+myList.head = new SLNode(10); //h->[10]->
+// myList.head.next = new SLNode(4);
+// myList.head.next.next = new SLNode(7);
+myList.addToBack(new SLNode(4));
+myList.addToBack(new SLNode(7));
+
+myList.printList();
