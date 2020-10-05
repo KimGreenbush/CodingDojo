f3088b6334b2cc3699a1b87adcb24445f2b338c0
Kim G <Kim_greenbush@icloud.com>
2020-10-05 04:12:16 -0700

    Finished Form Submission ASPNET

---
+++ C#DOTNET/Data_Structures/FormSubmission/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using FormSubmission.Models;
+
+namespace FormSubmission.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public ViewResult Index()
+        {
+            return View("Index");
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(User fromForm)
+        {
+            if (ModelState.IsValid)
+            {
+                User person = new User();
+                return RedirectToAction("NewUser", person);
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+
+        [HttpGet("newuser")]
+        public ActionResult NewUser()
+        {
+            return View("Success");
+        }
+    }
+}
+
