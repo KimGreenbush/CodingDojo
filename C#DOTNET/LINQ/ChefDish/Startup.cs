ce62447df4c5ff3041fa12ce53ecfc8ecef82699
Kim G <Kim_greenbush@icloud.com>
2020-10-10 14:25:00 -0700

    was missing a using statement, updated gitignore

--- C#DOTNET/LINQ/ChefDish/Startup.cs
+++ C#DOTNET/LINQ/ChefDish/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
 using ChefDish.Models;
