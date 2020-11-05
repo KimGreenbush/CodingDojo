c17efdc13d29b000c89f0fcd0744ffcee7302f2a
Kim G <Kim_greenbush@icloud.com>
2020-11-05 14:53:32 -0800

    working routing and api call refreshes

--- MERN/React/Routing/luke-apiwalker/src/App.js
+++ MERN/React/Routing/luke-apiwalker/src/App.js
@@ -1,11 +1,26 @@
-import './App.css';
+import "./App.css";
+import Profile from "./components/Profile";
+import Input from "./components/Input";
+import { Router, navigate } from "@reach/router";
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+    const [id, setId] = useState("");
+    const [thing, setThing] = useState("");
+
+    const updateSearch = e => {
+        e.preventDefault();
+        navigate(`/${thing}/${id}`);
+        setId("");
+    }
+
+    return <div className="App">
+        <Input id={id} setId={setId} thing={thing} setThing={setThing} updateSearch={updateSearch}/>
+        <Router>
+            <Profile path="/:thing/:id" />
+        </Router>
+    </div>;
 }
 
 export default App;
